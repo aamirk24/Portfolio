165c8d2cb291d6a40bde9e8c0ea697aa42b707ac
Aamir Khan <sc23a4k@leeds.ac.uk>
2026-08-11 23:00:46 +0100

    feat: establish portfolio visual foundation

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,7 @@ def test_homepage(client):
     response = client.get("/")
 
     assert response.status_code == 200
-    assert b"Portfolio under development" in response.data
+    assert b"I build software that makes complex information useful." in response.data
+    assert b"aamirk2405@gmail.com" in response.data
+    assert b"https://github.com/aamirk24" in response.data
+    assert b"https://www.linkedin.com/in/aamirkhan05/" in response.data
